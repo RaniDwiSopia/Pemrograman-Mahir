cc89507f90c343f23fb684b49bac47ccd7b88039
RaniDwiSopia <12350122946@students.uin-suska.ac.id>
2024-09-10 19:30:53 +0700

    Add files via upload

---
+++ RaniDwiSopia.java
@@ -0,0 +1,9 @@
+public class RaniDwiSopia {
+    public static void main(String[] args){
+        System.out.println("Rani Dwi Sopia");
+        System.out.println("12350122946");
+        System.out.println("Mahasiswa program studi Teknik Informatika");
+
+
+    }
+}
